28faec31d6be2ab0ae35cfdc58a3ea2d353b2861
arnavrajsingh19 <142911860+arnavrajsingh19@users.noreply.github.com>
2025-04-10 10:45:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://studymantra1.vercel.app"],
+    origin: ["https://studymantra1.vercel.app"],
     credentials: true,
   })
 );
