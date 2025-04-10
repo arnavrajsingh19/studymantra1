e2c781c517307ec8bed763d0e08868030d9bd006
arnavrajsingh19 <142911860+arnavrajsingh19@users.noreply.github.com>
2025-04-10 10:32:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,12 +22,11 @@ database.connect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
-
+  cors({
+    origin: ["http://localhost:3000", "https://studymantra1.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(
 	fileUpload({
 		useTempFiles:true,
