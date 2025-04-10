1a9dbf7f18725d86bb2462ba438283a91b83c061
arnavrajsingh19 <142911860+arnavrajsingh19@users.noreply.github.com>
2025-04-10 22:26:04 +0530

    Update Course.js

--- server/routes/Course.js
+++ server/routes/Course.js
@@ -91,6 +91,7 @@ router.post("/updateCourseProgress", auth, isStudent, updateCourseProgress);
 // TODO: Put IsAdmin Middleware here
 router.post("/createCategory", auth, isAdmin, createCategory)
 router.get("/showAllCategories", showAllCategories)
+router.get("/categories", showAllCategories)
 router.post("/getCategoryPageDetails", categoryPageDetails)
 
 // ********************************************************************************************************
